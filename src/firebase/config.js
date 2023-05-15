d57ea477713321b425d7f18eb43b4fb80a34dfa5
Ayush Tripathi <113000672+Ayushtri441@users.noreply.github.com>
2023-05-16 00:26:42 +0530

    Initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+import 'firebase/compat/storage'
+/*
+const firebaseConfig = {
+    apiKey: "AIzaSyDQJ-lEVObTeU0tMHZqOUKOyOGS5E2fDbk",
+   // authDomain: "olx-sijeesh.firebaseapp.com",
+    projectId: "my-olx-mini",
+    storageBucket: "gs://my-olx-mini.appspot.com",
+    messagingSenderId: "811770900869",
+    appId: "1:811770900869:web:fac06c56975ab00d8694ec",
+   // measurementId: "G-4ELNR9DJHL"
+  };
+*/
+  const firebaseConfig = {
+    apiKey: "AIzaSyDQJ-lEVObTeU0tMHZqOUKOyOGS5E2fDbk",
+    authDomain: "my-olx-mini.firebaseapp.com",
+    databaseURL: "https://my-olx-mini-default-rtdb.firebaseio.com",
+    projectId: "my-olx-mini",
+    storageBucket: "my-olx-mini.appspot.com",
+    messagingSenderId: "811770900869",
+    appId: "1:811770900869:web:fac06c56975ab00d8694ec",
+    measurementId: "G-XVCB8PRS7P"
+  };
+  export const Firebase= firebase.initializeApp(firebaseConfig)
